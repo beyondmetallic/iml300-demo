1802799080cfbf4d2235868898cc5422b3173fc0
beyondmetallic <jmmirand@usc.edu>
2022-04-12 20:18:46 -0700

    add project 3

---
+++ project-3/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+//    $(function () {
+//        $("#computer-plants, #plant1, #plant2, #plant3, #plant4, #p2,.pop-up").draggable();
+//    });
+//    
+    $(function () {
+        $(".plannt").draggable();
+    });
