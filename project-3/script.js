0bed0700221a67aa0af048aa84212f96ed6c08ae
beyondmetallic <jmmirand@usc.edu>
2022-04-26 02:09:37 -0700

    p3

--- project-3/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-//    $(function () {
-//        $("#computer-plants, #plant1, #plant2, #plant3, #plant4, #p2,.pop-up").draggable();
-//    });
-//    
-    $(function () {
-        $(".plannt").draggable();
-    });
